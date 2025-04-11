129d2275f397c9f4e2dee2fd40a0589b59e0930d
not-josue <129870578+not-josue@users.noreply.github.com>
2025-04-11 19:11:58 -0400

    added timestamp to comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,19 @@
 // CSS
 import "./css/Comment.css";
 
-export default function Comment({ name, text }) {
+export default function Comment({ name, text, timestamp }) {
+  // Formats the date
+  const FormatDate = (data) => {
+    const date = new Date(data);
+    const parse = { month: "long", day: "numeric", year: "numeric" };
+    return date.toLocaleDateString("en-US", parse);
+  };
+
   return (
     <div className="user-comment">
       <p className="user-comment-name">
         <b>{name}</b>
+        {timestamp && <small>{FormatDate(timestamp)}</small>}
       </p>
       <p className="user-comment-text">{text}</p>
     </div>
